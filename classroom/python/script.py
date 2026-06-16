e0f04df9828cc12873692026971c8abe16eafb08
Aramo03 <aram1023aram@gmail.com>
2026-06-16 16:02:49 +0400

    pythone json

--- classroom/python/script.py
+++ classroom/python/script.py
@@ -1,6 +1,25 @@
-a = int(input("first number: "))
-b = int(input("first second number: "))
+import json
 
-sum = a + b
+user_login = input("Login: ")
+user_password = input("Password: ")
 
-print(sum)
+new_user = {
+    "login": user_login,
+    "password": user_password
+}
+
+
+try:
+    with open("data.json", "r") as file:
+        data = json.load(file)
+except:
+    data = []
+
+
+data.append(new_user)
+
+
+with open("data.json", "w") as file:
+    json.dump(data, file, indent=4)
+
+print("Saved")
