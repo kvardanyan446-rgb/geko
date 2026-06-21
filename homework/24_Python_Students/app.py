7d405fc95312b850f3481a07f71bcc2c04b8e0fb
Karen Vardanyan <kvardanyan446@gmail.com>
2026-06-22 02:26:47 +0400

    24_Python_Students

--- homework/24_Python_Students/app.py
+++ homework/24_Python_Students/app.py
@@ -1,7 +1,23 @@
 students = {
-    "aram" : [90, 85 100],
-    "anna" : [75, 88],
-    "karen" : [] 
-    
-    }
-    yntrel = input()
+    "Aram" : [90, 85, 100],
+    "Anna" : [75, 88],
+    "Karen" : []
+}
+
+
+
+while True:
+    print("------menyu-----")
+    print("Avelacnel usanox  ")
+    print("Avelacnel gnahatakan ")
+    print("Cuyc tal bolor usanoxnerin ")
+    print("cuyc tal vijakacrutyuny ")
+    print("nshel lavaguyn usanoxin ")
+    print("jnjel usanoxin ")
+    print("jnjel gnahatakan ")
+    print("sortavorel usanoxnerin yst mijini ")
+    print("pntrel mijin cacr usanoxin ")
+    print("elqq ")
+
+choice = input("\nyntreq gorcoxutyuny 0-9").strip()
+
