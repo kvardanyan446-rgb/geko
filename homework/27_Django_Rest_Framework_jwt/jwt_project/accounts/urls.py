3447f8c5bb6e6f4e74218696410b736c767b2b26
Karen Vardanyan <kvardanyan446@gmail.com>
2026-07-18 03:03:32 +0400

    27

---
+++ homework/27_Django_Rest_Framework_jwt/jwt_project/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import register_view
+
+
+urlpatterns = [
+    path(
+        "register/",
+        register_view
+    ),
+]
---
+++ homework/27_Django_Rest_Framework_jwt/jwt_project/accounts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import RegisterSerializer
+@api_view(["POST"])
+def register_view(request):
+
+    serializer = RegisterSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+        return Response(
+            {
+                "message": "User created successfully"
+            }
+        )
+
+    return Response(
+        serializer.errors
+    )
+
+# Create your views here.
